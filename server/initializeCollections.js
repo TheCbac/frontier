4344acb77948e80399c27f017b9b9f97d354f507
--global user.name "Craig Cheney <ccheney@mit.edu>
2015-04-18 17:00:59 -0400

    Subscription properly working
    Got the publish/subscription working for the admin page

--- server/initializeCollections.js
+++ server/initializeCollections.js
@@ -1,19 +1,30 @@
 if (emailCollection.find().count()===0){
-	
+	console.log("insert initial email");
 	emailCollection.insert({
 		email:"ccheney@mit.edu"
+	}, function(error, id){
+		console.log(error, id);
 	});
 }
 
-Meteor.publish('Emails', function(){
-	user = Meteor.users.findOne({_id:userId});
-	if (user.profile.role=='admin'){
-		return emailCollection.find();
-	}
-	else{
-		return [];
+Meteor.publish('emailList', function(){
+	user = Meteor.users.findOne({_id:this.userId});
+	// debugger
+
+	// return emailCollection.find();
+	// console.log(Meteor.user().profile.role);
+	if (user){
+		if (user.profile.role=='admin'){
+			return emailCollection.find();
+		}
+		else{
+
+			return [];
+		}
 	}
 });
+
+
 
 
 if (Meteor.users.find().count() ===0){
